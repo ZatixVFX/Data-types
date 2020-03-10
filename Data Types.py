b1989352f90966b286f9ba2eb1b39a625e774e20
Junai Salie <ZatixVFX@users.noreply.github.com>
2020-03-10 01:10:30 -0700

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,33 @@
+# Declaring a string variable X
+x = "Life Choices Coding Academy"
+# Displaying Data Type of X
+print(type(x))
+# Displaying the position of letter
+print(x[4:5])
+# Declaring Integer
+intNum1 = 5
+# Declaring Float
+intNum2 = 5.8
+# Declaring Complex
+intNum3 = 4e6j
+print(type(intNum1),type(intNum2),type(intNum3))
+# Declaring a List
+mylist = [23,82,42,33]
+# Declaring a insert
+mylist.insert(4,24)
+print(mylist)
+# Declaring a list
+print(mylist)
+# Declaring a reverse
+mylist.reverse()
+print(mylist)
+# Declaring a sort
+mylist.sort()
+print(mylist)
+# Declaring a Data type
+print(type(mylist))
+Cars = {"Aston Martin","VW","Ford","BMW","Nissan"}
+print(Cars)
+Cars1 = ("Aston Martin","VW","Ford","BMW","Nissan")
+print(type(Cars),type(Cars1))
+
